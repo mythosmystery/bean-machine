88c75a3533cea96f993624fb46b33951f90fbdf6
Hunter Barton <axiysfire@gmail.com>
2023-06-20 23:41:30 -0500

    fix: profile

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -4,5 +4,6 @@ import type { PageServerLoad } from './$types';
 export const load = (async ({ parent }) => {
 	const { session } = await parent();
 	if (!session) throw redirect(302, '/login');
+
 	return { hello: 'World' };
 }) satisfies PageServerLoad;
